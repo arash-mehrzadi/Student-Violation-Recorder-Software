828c2c0205ee261354ea19667a7d771df171fe03
arash mehrzadi <48579846+arash-mehrzadi@users.noreply.github.com>
2020-05-31 04:08:12 -0700

    Add files via upload

---
+++ Formsetting.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace uniproject__SVR_
+{
+    partial class Formsetting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtviolation = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtnewpas = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtnewpasstwo = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnpassword = new System.Windows.Forms.Button();
+            this.txtdeleteviolation = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btndeleteviolation = new FontAwesome.Sharp.IconButton();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(454, 62);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(104, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "افزودن تخلف :";
+            // 
+            // txtviolation
+            // 
+            this.txtviolation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtviolation.Location = new System.Drawing.Point(23, 64);
+            this.txtviolation.Name = "txtviolation";
+            this.txtviolation.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtviolation.Size = new System.Drawing.Size(416, 20);
+            this.txtviolation.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(23, 224);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(535, 10);
+            this.panel1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(409, 253);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(129, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "تغییر رمز دسترسی";
+            // 
+            // txtpass
+            // 
+            this.txtpass.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtpass.Location = new System.Drawing.Point(239, 289);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtpass.Size = new System.Drawing.Size(200, 20);
+            this.txtpass.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(454, 289);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label5.Size = new System.Drawing.Size(79, 22);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "رمز فعلی :";
+            // 
+            // txtnewpas
+            // 
+            this.txtnewpas.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtnewpas.Location = new System.Drawing.Point(239, 315);
+            this.txtnewpas.Name = "txtnewpas";
+            this.txtnewpas.PasswordChar = '*';
+            this.txtnewpas.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtnewpas.Size = new System.Drawing.Size(200, 20);
+            this.txtnewpas.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(454, 315);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(80, 22);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "رمز جدید :";
+            // 
+            // txtnewpasstwo
+            // 
+            this.txtnewpasstwo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtnewpasstwo.Location = new System.Drawing.Point(239, 341);
+            this.txtnewpasstwo.Name = "txtnewpasstwo";
+            this.txtnewpasstwo.PasswordChar = '*';
+            this.txtnewpasstwo.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtnewpasstwo.Size = new System.Drawing.Size(200, 20);
+            this.txtnewpasstwo.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(454, 341);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label7.Size = new System.Drawing.Size(80, 22);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "رمز جدید :";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(23, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 34);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "ثبت";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnpassword
+            // 
+            this.btnpassword.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnpassword.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnpassword.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnpassword.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnpassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnpassword.Location = new System.Drawing.Point(23, 333);
+            this.btnpassword.Name = "btnpassword";
+            this.btnpassword.Size = new System.Drawing.Size(129, 34);
+            this.btnpassword.TabIndex = 14;
+            this.btnpassword.Text = "ثبت";
+            this.btnpassword.UseVisualStyleBackColor = true;
+            this.btnpassword.Click += new System.EventHandler(this.btnpassword_Click);
+            // 
+            // txtdeleteviolation
+            // 
+            this.txtdeleteviolation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtdeleteviolation.Location = new System.Drawing.Point(23, 130);
+            this.txtdeleteviolation.Name = "txtdeleteviolation";
+            this.txtdeleteviolation.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtdeleteviolation.Size = new System.Drawing.Size(416, 20);
+            this.txtdeleteviolation.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(467, 130);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(91, 22);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "حذف تخلف :";
+            // 
+            // btndeleteviolation
+            // 
+            this.btndeleteviolation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btndeleteviolation.FlatAppearance.BorderColor = System.Drawing.Color.DarkRed;
+            this.btndeleteviolation.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkRed;
+            this.btndeleteviolation.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkRed;
+            this.btndeleteviolation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btndeleteviolation.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btndeleteviolation.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndeleteviolation.ForeColor = System.Drawing.Color.Black;
+            this.btndeleteviolation.IconChar = FontAwesome.Sharp.IconChar.UserTimes;
+            this.btndeleteviolation.IconColor = System.Drawing.Color.Black;
+            this.btndeleteviolation.IconSize = 30;
+            this.btndeleteviolation.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btndeleteviolation.Location = new System.Drawing.Point(23, 156);
+            this.btndeleteviolation.Name = "btndeleteviolation";
+            this.btndeleteviolation.Rotation = 0D;
+            this.btndeleteviolation.Size = new System.Drawing.Size(129, 34);
+            this.btndeleteviolation.TabIndex = 23;
+            this.btndeleteviolation.Text = "حذف";
+            this.btndeleteviolation.UseVisualStyleBackColor = true;
+            this.btndeleteviolation.Click += new System.EventHandler(this.btndeleteviolation_Click);
+            // 
+            // Formsetting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(579, 393);
+            this.Controls.Add(this.btndeleteviolation);
+            this.Controls.Add(this.txtdeleteviolation);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnpassword);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtnewpasstwo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtnewpas);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtviolation);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Formsetting";
+            this.Text = "Formsetting";
+            this.Load += new System.EventHandler(this.Formsetting_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtviolation;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtnewpas;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtnewpasstwo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnpassword;
+        private System.Windows.Forms.TextBox txtdeleteviolation;
+        private System.Windows.Forms.Label label3;
+        private FontAwesome.Sharp.IconButton btndeleteviolation;
+    }
+}
