4fcaa812d5e6bbde53a81116592aff138d37d8e4
arash mehrzadi <48579846+arash-mehrzadi@users.noreply.github.com>
2020-05-31 04:02:58 -0700

    Add files via upload

---
+++ FormAdd.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace uniproject__SVR_
+{
+    partial class FormAdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtfirstname = new System.Windows.Forms.TextBox();
+            this.txtssn = new System.Windows.Forms.TextBox();
+            this.txtlastname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtexplanation = new System.Windows.Forms.TextBox();
+            this.txtyear = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtday = new System.Windows.Forms.TextBox();
+            this.txtmonth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnadd = new FontAwesome.Sharp.IconButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtviolationsearch = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(541, -9);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 30, 12, 0);
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(50, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "نام :";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(450, 83);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(129, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "شماره دانشجویی :";
+            // 
+            // txtfirstname
+            // 
+            this.txtfirstname.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtfirstname.Location = new System.Drawing.Point(250, 19);
+            this.txtfirstname.Name = "txtfirstname";
+            this.txtfirstname.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtfirstname.Size = new System.Drawing.Size(148, 20);
+            this.txtfirstname.TabIndex = 0;
+            // 
+            // txtssn
+            // 
+            this.txtssn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtssn.Location = new System.Drawing.Point(250, 82);
+            this.txtssn.Name = "txtssn";
+            this.txtssn.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtssn.Size = new System.Drawing.Size(148, 20);
+            this.txtssn.TabIndex = 2;
+            // 
+            // txtlastname
+            // 
+            this.txtlastname.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtlastname.Location = new System.Drawing.Point(250, 51);
+            this.txtlastname.Name = "txtlastname";
+            this.txtlastname.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtlastname.Size = new System.Drawing.Size(148, 20);
+            this.txtlastname.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(160, 18);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(84, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "نوع تخلف :";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(488, 153);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label5.Size = new System.Drawing.Size(91, 22);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "شرح تخلف :";
+            // 
+            // txtexplanation
+            // 
+            this.txtexplanation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtexplanation.Location = new System.Drawing.Point(246, 178);
+            this.txtexplanation.Multiline = true;
+            this.txtexplanation.Name = "txtexplanation";
+            this.txtexplanation.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtexplanation.Size = new System.Drawing.Size(333, 178);
+            this.txtexplanation.TabIndex = 6;
+            // 
+            // txtyear
+            // 
+            this.txtyear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtyear.Location = new System.Drawing.Point(249, 121);
+            this.txtyear.Name = "txtyear";
+            this.txtyear.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtyear.Size = new System.Drawing.Size(53, 20);
+            this.txtyear.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(519, 122);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label6.Size = new System.Drawing.Size(60, 22);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "تاریخ : ";
+            // 
+            // txtday
+            // 
+            this.txtday.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtday.Location = new System.Drawing.Point(361, 121);
+            this.txtday.Name = "txtday";
+            this.txtday.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtday.Size = new System.Drawing.Size(37, 20);
+            this.txtday.TabIndex = 3;
+            // 
+            // txtmonth
+            // 
+            this.txtmonth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtmonth.Location = new System.Drawing.Point(318, 121);
+            this.txtmonth.Name = "txtmonth";
+            this.txtmonth.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtmonth.Size = new System.Drawing.Size(37, 20);
+            this.txtmonth.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(479, 52);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(100, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "نام خانوادگی :";
+            // 
+            // btnadd
+            // 
+            this.btnadd.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.btnadd.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.btnadd.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.btnadd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnadd.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btnadd.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadd.ForeColor = System.Drawing.Color.Black;
+            this.btnadd.IconChar = FontAwesome.Sharp.IconChar.Check;
+            this.btnadd.IconColor = System.Drawing.Color.Black;
+            this.btnadd.IconSize = 30;
+            this.btnadd.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnadd.Location = new System.Drawing.Point(12, 362);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Rotation = 0D;
+            this.btnadd.Size = new System.Drawing.Size(120, 29);
+            this.btnadd.TabIndex = 15;
+            this.btnadd.Text = "ثبت";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dataGridView1.Size = new System.Drawing.Size(241, 305);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // txtviolationsearch
+            // 
+            this.txtviolationsearch.Location = new System.Drawing.Point(6, 18);
+            this.txtviolationsearch.Name = "txtviolationsearch";
+            this.txtviolationsearch.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtviolationsearch.Size = new System.Drawing.Size(148, 20);
+            this.txtviolationsearch.TabIndex = 7;
+            this.txtviolationsearch.TextChanged += new System.EventHandler(this.txtviolationsearch_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(373, 101);
+            this.label9.Name = "label9";
+            this.label9.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label9.Size = new System.Drawing.Size(30, 19);
+            this.label9.TabIndex = 28;
+            this.label9.Text = "روز";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(330, 101);
+            this.label8.Name = "label8";
+            this.label8.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label8.Size = new System.Drawing.Size(25, 19);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "ماه";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(273, 103);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label7.Size = new System.Drawing.Size(30, 19);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "سال";
+            // 
+            // FormAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(591, 403);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtviolationsearch);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.txtmonth);
+            this.Controls.Add(this.txtday);
+            this.Controls.Add(this.txtyear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtexplanation);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtlastname);
+            this.Controls.Add(this.txtssn);
+            this.Controls.Add(this.txtfirstname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormAdd";
+            this.Text = "FormAdd";
+            this.Load += new System.EventHandler(this.FormAdd_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtfirstname;
+        private System.Windows.Forms.TextBox txtssn;
+        private System.Windows.Forms.TextBox txtlastname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtexplanation;
+        private System.Windows.Forms.TextBox txtyear;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtday;
+        private System.Windows.Forms.TextBox txtmonth;
+        private System.Windows.Forms.Label label3;
+        private FontAwesome.Sharp.IconButton btnadd;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtviolationsearch;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+    }
+}
